7cdb56e0795a65b70383d86cf6bcb76781e2715a
Orochyy <yura333619@gmail.com>
2023-06-28 23:13:58 +0300

    refactoring

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ func HomeEndpoint(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", HomeEndpoint)
+	fmt.Println("Server is running on port 3000")
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Server is running on port 3000")
 }
