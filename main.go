c42731fcf979675779afdf2212801f8489fe83d4
Orochyy <yura333619@gmail.com>
2023-06-28 23:30:34 +0300

    delete ports

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func HomeEndpoint(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", HomeEndpoint)
 	fmt.Println("Server is running on port 3000")
-	if err := http.ListenAndServe(":3000", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
